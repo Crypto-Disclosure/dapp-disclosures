edeca25f0cf2528f8354f7e5c82bac897d486e70
crashtestdumb1 <crashtestdumb@gmail.com>
2023-08-06 15:02:02 -0500

    Added Georestrictions to the site

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from './components/Header';
 import Platforms from './components/Platforms';
 import Documentation from './components/Documentation';
 import Assets from './components/Assets';
+import Georestrictions from './components/Georestrictions';
+
 import './App.css';
 
 function App() {
   const [daoData, setDaoData] = useState(null);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  /* const [isDarkMode, setIsDarkMode] = useState(false);*/
 
   useEffect(() => {
     // Fetch the data from the GitHub link
@@ -17,6 +19,7 @@ function App() {
       .catch(error => console.error('Error fetching data:', error));
   }, []);
 
+  /*
   useEffect(() => {
     if (isDarkMode) {
       document.body.classList.add('dark-mode');
@@ -24,6 +27,7 @@ function App() {
       document.body.classList.remove('dark-mode');
     }
   }, [isDarkMode]);
+  */
 
   if (!daoData) {
     return <div>Loading...</div>; // Display a loading message while waiting for the data to be fetched
@@ -40,6 +44,7 @@ function App() {
       <Platforms platforms={daoData.platforms} />
       <Documentation documentation={daoData.documentation} />
       <Assets assets={daoData.assets} />
+      <Georestrictions georestrictions={daoData.documentation[0].georestrictions} />
       {/* Other components will go here, and you will pass the appropriate data to them as props */}
     </div>
   );
